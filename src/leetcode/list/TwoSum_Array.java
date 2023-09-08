24658c8060e9dada81df965bb81ee73add216dfc
rootboy1 <ravimulani101@gmail.com>
2023-09-08 16:25:32 -0400

    Add -> 1. Two Sum

---
+++ src/leetcode/list/TwoSum_Array.java
@@ -0,0 +1,32 @@
+package leetcode.list;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TwoSum_Array {
+	public static void main(String[] args) {
+		 int[] nums = {2, 7, 11, 15, 3, 8,1};
+	        int target = 9;
+	       
+	        List<int[]> pairs = twoSum(nums, target);
+	        if (!pairs.isEmpty()) {
+	            for (int[] pair : pairs) {
+	                System.out.println("Pairs: " + pair[0] + ", " + pair[1]);
+	            }
+	        } else {
+	            System.out.println("No valid pairs found.");
+	        }
+	    }
+
+	 public static List<int[]> twoSum(int[] nums, int target) {
+	        List<int[]> result = new ArrayList<>();
+	        for (int i = 0; i < nums.length - 1; i++) {
+	            for (int j = i + 1; j < nums.length; j++) {
+	                if (nums[i] + nums[j] == target) {
+	                    result.add(new int[]{i, j});
+	                }
+	            }
+	        }
+
+	        return result;
+	    }
+}
