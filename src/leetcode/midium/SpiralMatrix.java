c59e9dc6fd3839522dc0ae495b5142d3bc3819bc
Ravi Mulani <ravimulani101@gmail.com>
2023-09-10 21:59:55 -0400

    54.-> Spiral Matrix

---
+++ src/leetcode/midium/SpiralMatrix.java
@@ -0,0 +1,67 @@
+package leetcode.midium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SpiralMatrix {
+
+	public static void main(String[] args) {
+
+		int[][] matrix = {
+	            {1, 2, 3},
+	            {4, 5, 6},
+	            {7, 8, 9}
+	        };
+	        List<Integer> result = spiralOrder(matrix);
+	        System.out.println(result);
+	}
+	
+	public static List<Integer> spiralOrder(int[][] matrix) {
+
+        List<Integer> result = new ArrayList<>();
+        
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return result;
+        }
+        
+        int top = 0;
+        int bottom = matrix.length - 1;
+        int left = 0;
+        int right = matrix[0].length - 1;
+        
+        while (top <= bottom && left <= right) {
+            // Traverse from left to right along the top row
+            for (int i = left; i <= right; i++) {
+                result.add(matrix[top][i]);
+            }
+            top++;
+            
+            // Traverse from top to bottom along the rightmost column
+            for (int i = top; i <= bottom; i++) {
+                result.add(matrix[i][right]);
+            }
+            right--;
+            
+            // Check if there's more to traverse
+            if (top <= bottom) {
+                // Traverse from right to left along the bottom row
+                for (int i = right; i >= left; i--) {
+                    result.add(matrix[bottom][i]);
+                }
+                bottom--;
+            }
+            
+            // Check if there's more to traverse
+            if (left <= right) {
+                // Traverse from bottom to top along the leftmost column
+                for (int i = bottom; i >= top; i--) {
+                    result.add(matrix[i][left]);
+                }
+                left++;
+            }
+        }
+        
+        return result;
+    }
+
+}
