6a684201f6a79690c7d3d6f73e40a1c08b7f7dbb
Ravi Mulani <ravimulani101@gmail.com>
2023-09-16 16:17:35 -0400

    print result using nested for loop

--- src/leetcode/list/SingleNumber.java
+++ src/leetcode/list/SingleNumber.java
@@ -4,7 +4,8 @@ public class SingleNumber {
 
 	public static void main(String[] args) {
 		int nums[] = {4,1,2,1,2};
-		int result = singleNumber(nums);
+//		int result = singleNumber(nums);
+		int result = singleNumbers(nums);
 		System.out.println(result);
 
 	}
@@ -15,5 +16,23 @@ public class SingleNumber {
             result ^= num;
         }
         return result;
-}
+	}
+	
+	//using nested for loop	
+	public static int singleNumbers(int[] nums) {
+        for(int i = 0 ; i <nums.length; i++){
+            int count = 0;
+            for(int j = 0; j < nums.length ; j++){
+                if(nums[i] == nums[j]){
+                    if(i!=j){
+                    count ++;
+                    }
+                }
+            }
+            if(count == 0){
+                return nums[i];
+            }
+        }
+        return -1;
+    }
 }
