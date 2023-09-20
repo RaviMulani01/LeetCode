1945c45f4f5e3957e07d75192bd5a039ebc1502d
Ravi Mulani <ravimulani101@gmail.com>
2023-09-19 23:23:06 -0400

    66. Plus One

---
+++ src/leetcode/list/PlusOne.java
@@ -0,0 +1,30 @@
+package leetcode.list;
+
+public class PlusOne {
+
+	public static void main(String[] args) {
+		int digits[] = {1,2,3};
+		int[] result = plusOne(digits);
+
+		for(int r:result)
+			System.out.print(r + " ");
+	}
+	public static int[] plusOne(int[] digits) {
+	    int carry = 1;
+	    for (int i = digits.length - 1; i >= 0; i--) {
+	        int currentSum = digits[i] + carry;
+	        digits[i] = currentSum % 10;
+	        carry = currentSum / 10;
+	    }
+
+	    if (carry != 0) {
+	        int[] result = new int[digits.length + 1];
+	        result[0] = carry;
+	        System.arraycopy(digits, 0, result, 1, digits.length);
+	        return result;
+	    } else {
+	        return digits;
+	    }
+	    }
+
+}
